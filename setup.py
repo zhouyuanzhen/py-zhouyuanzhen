5732473376dfc49219e4fb5a58788d92e6a9c1e6
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2020-07-08 14:44:42 +0800

    setup.py update

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from setuptools import setup, find_packages
 import io
 import re
 
-
 with io.open('README.md', 'rt', encoding='utf8') as f:
     lines = f.read()
     # longdesc = re.sub(r'\[<img(.+)https://github.com/zhouyuanzhen\)\n', '', lines)
     longdesc = lines
 
-
 setup(
     name='zhouyuanzhen',
     license='Apache License 2.0',
-    version='0.2.0',
+    version='0.2.2',
     maintainer='Yuanzhen Zhou',
     maintainer_email='szrednick@gmail.com',
     packages=find_packages(include=['zyz', 'zyz.*']),
