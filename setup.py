e371bc359847ff07cca453c69ad3402657ff67d4
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2020-07-08 13:23:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 import io
+import re
+
+longdesc = ''
 
 with io.open('README.md', 'rt', encoding='utf8') as f:
-    readme = f.read()
+    lines = f.readlines()
+    for line in lines:
+        if len(re.findall('align="*', line)) == 0:
+            longdesc += line
+
 
 setup(
     name='zhouyuanzhen',
     license='Apache License 2.0',
-    version='0.0.1',
+    version='0.1.0',
     maintainer='Yuanzhen Zhou',
     maintainer_email='szrednick@gmail.com',
     packages=find_packages(include=['zyz', 'zyz.*']),
@@ -17,9 +24,9 @@ setup(
     author='Yuanzhen Zhou',
     author_email='szrednick@gmail.com',
     description='A PyPI project of zhouyuanzhen',
-    long_description=readme,
+    long_description=longdesc,
     install_requires=[],
-        extras_require={
+    extras_require={
         'test': [
             'pytest',
             'coverage',
